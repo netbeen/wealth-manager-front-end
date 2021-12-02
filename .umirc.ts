3d540ca4ac1f4fa1c06e3a4ddb3dad6d13f19df0
Yang Yang <yangyang.jeff@bytedance.com>
2021-12-02 21:28:06 +0800

    feat: add google verification meta

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,11 @@ import { IConfig } from 'umi-types';
 const config: IConfig = {
   antd: { mobile: false },
   title:'Wealth Manager',
-  links: [{ rel: 'icon', href: '/img/logo.svg' }]
-
+  links: [{ rel: 'icon', href: '/img/logo.svg' }],
+  metas: [{
+    name: 'google-site-verification',
+    content: 'nfOPr4G-D6vPaud2_lZtu7JpzA6FZ71Zy3XV-ekCL8w',
+  }],
 };
 
 export default config;
