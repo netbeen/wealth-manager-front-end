48ca172dee92e617939d442c42ff9751ea8b1d63
Yang Yang <yangyang.jeff@bytedance.com>
2021-12-02 21:40:38 +0800

    feat: enrich more meta

--- .umirc.ts
+++ .umirc.ts
@@ -3,11 +3,20 @@ import { IConfig } from 'umi-types';
 // ref: https://umijs.org/config/
 const config: IConfig = {
   antd: { mobile: false },
-  title:'Wealth Manager',
-  links: [{ rel: 'icon', href: '/img/logo.svg' }],
+  title: 'Wealth Manager',
+  links: [{
+    rel: 'icon',
+    href: '/img/logo.svg',
+  }],
   metas: [{
     name: 'google-site-verification',
     content: 'nfOPr4G-D6vPaud2_lZtu7JpzA6FZ71Zy3XV-ekCL8w',
+  }, {
+    name: 'keywords',
+    content: 'fund, wealth, manager',
+  }, {
+    name: 'description',
+    content: '一站式基金投资管理平台',
   }],
   webpack5: {},
   mfsu: {},
