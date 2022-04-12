036ce8324824db23d342c83ec1a5685973198c43
Yang Yang <yangyang.jeff@bytedance.com>
2022-04-12 22:38:43 +0800

    fix: add loading top while transactions loading

--- src/pages/fund/transactionList.tsx
+++ src/pages/fund/transactionList.tsx
@@ -27,7 +27,6 @@ export default function() {
     }
     return (await fetchBasicInfoUnitPriceSplitDividendByIdentifier(transactionSets.map(transactionSet => transactionSet.target)))
   }, { refreshDeps: [transactionSets] });
-  console.log('fourBasicInfo', fourBasicInfo);
 
   const { data: transactions, loading: transactionsLoading } = useRequest(async () => {
     if(!transactionSets){
@@ -38,73 +37,76 @@ export default function() {
       .flat(1)
       .sort((a, b)=>(a.date.isAfter(b.date) ? -1 :  1))
   }, { refreshDeps: [transactionSets] });
-  console.log('transactions', transactions);
 
   const mainContent = useMemo(()=>{
+    let loading = true;
+
     if(
-      !transactions || !fourBasicInfo ||
-      !fourBasicInfo.basicInfos || fourBasicInfo.basicInfos.length === 0 ||
-      !fourBasicInfo.unitPrices || fourBasicInfo.unitPrices.length === 0
+      transactions && fourBasicInfo &&
+      fourBasicInfo.basicInfos?.length > 0 &&
+      fourBasicInfo.unitPrices?.length > 0 &&
+      transactionSets
     ){
-      return null;
+      loading = false;
     }
+
+    const dataSource = loading ? [] : transactions.map(transaction => {
+      const targetTransactionSet = transactionSets.find(transactionSet => transactionSet._id === transaction.transactionSet)
+      if(!targetTransactionSet){
+        return null;
+      }
+      const transactionIndex= transactionSets.map(item => item._id).indexOf(targetTransactionSet._id);
+      if(transactionIndex === -1){
+        console.error('find targetTransactionSet failed', targetTransactionSet, transactionSets)
+      }
+      const targetBasicInfo = fourBasicInfo.basicInfos[transactionIndex];
+      const targetUnitPrices = fourBasicInfo.unitPrices[transactionIndex];
+      const targetUnitPrice = targetUnitPrices.find(item => item.date.isSame(transaction.date))?.price ?? 0;
+
+      return ({
+        name: targetBasicInfo?.name,
+        date: transaction.date.format('YYYY-MM-DD'),
+        direction: transaction.direction,
+        transactionValue: Math.round(((targetUnitPrice * transaction.volume) + transaction.commission) * 10)/10,
+      })
+    }).filter(item => item);
+
     return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
-      {Array.isArray(transactionSets) &&
-        <AntdBaseTable
-          dataSource={transactions.map(transaction => {
-            const targetTransactionSet = transactionSets.find(transactionSet => transactionSet._id === transaction.transactionSet)
-            if(!targetTransactionSet){
-              return null;
-            }
-            const transactionIndex= transactionSets.map(item => item._id).indexOf(targetTransactionSet._id);
-            if(transactionIndex === -1){
-              console.error('find targetTransactionSet failed', targetTransactionSet, transactionSets)
-            }
-            const targetBasicInfo = fourBasicInfo.basicInfos[transactionIndex];
-            const targetUnitPrices = fourBasicInfo.unitPrices[transactionIndex];
-            const targetUnitPrice = targetUnitPrices.find(item => item.date.isSame(transaction.date))?.price ?? 0;
-
-            return ({
-              name: targetBasicInfo?.name,
-              date: transaction.date.format('YYYY-MM-DD'),
-              direction: transaction.direction,
-              transactionValue: Math.round(((targetUnitPrice * transaction.volume) + transaction.commission) * 10)/10,
-            })
-          }).filter(item => item)}
-          columns={[{
-            code: 'name',
-            name: '基金名称',
-            width: 150,
-            align: 'left',
-          }, {
-            code: 'date',
-            name: '交易日期',
-            width: 100,
-            align: 'right',
-          }, {
-            code: 'date',
-            name: <div><div>交易方向</div><div>交易金额</div></div>,
-            width: 100,
-            align: 'right',
-            render: (value: any, record: any) => (
-              <div>
-                <div style={{color: record.direction === TRANSACTION_DIRECTION.BUY ? COLOR.LossMaking : COLOR.Profitable}}>
-                  {record.direction === TRANSACTION_DIRECTION.BUY ? '买入' : '卖出'}
-                </div>
-                <div>
-                  {Intl.NumberFormat('en-US', {
-                    maximumFractionDigits: 2,
-                    minimumFractionDigits: 2
-                  }).format(record.transactionValue)}
-                </div>
+      <AntdBaseTable
+        dataSource={dataSource}
+        columns={[{
+          code: 'name',
+          name: '基金名称',
+          width: 150,
+          align: 'left',
+        }, {
+          code: 'date',
+          name: '交易日期',
+          width: 100,
+          align: 'right',
+        }, {
+          code: 'date',
+          name: <div><div>交易方向</div><div>交易金额</div></div>,
+          width: 100,
+          align: 'right',
+          render: (value: any, record: any) => (
+            <div>
+              <div style={{color: record.direction === TRANSACTION_DIRECTION.BUY ? COLOR.LossMaking : COLOR.Profitable}}>
+                {record.direction === TRANSACTION_DIRECTION.BUY ? '买入' : '卖出'}
               </div>
-            )
-          }]}
-          isStickyHeader={false}
-          isLoading={transactionSetsLoading || transactionsLoading || fourBasicInfoLoading}
-        />
-      }
+              <div>
+                {Intl.NumberFormat('en-US', {
+                  maximumFractionDigits: 2,
+                  minimumFractionDigits: 2
+                }).format(record.transactionValue)}
+              </div>
+            </div>
+          )
+        }]}
+        isStickyHeader={false}
+        isLoading={transactionSetsLoading || transactionsLoading || fourBasicInfoLoading}
+      />
     </div>
   )},[transactionSets, transactions, transactionSetsLoading, transactionsLoading, fourBasicInfo, fourBasicInfoLoading]);
 
