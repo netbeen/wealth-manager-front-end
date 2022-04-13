03d7037cf01bb801402db3461f343d8d1cf38da0
Yang Yang <yangyang.jeff@bytedance.com>
2022-04-13 13:54:44 +0800

    refactor: eslint 0 error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,26 @@
 /* eslint-disable no-undef,no-restricted-globals */
 self.addEventListener('error', (e) => {
-  self.clients.matchAll()
-    .then((clients) => {
-      if (clients && clients.length) {
-        clients[0].postMessage({
-          type: 'ERROR',
-          msg: e.message || null,
-          stack: e.error ? e.error.stack : null,
-        });
-      }
-    });
+  self.clients.matchAll().then((clients) => {
+    if (clients && clients.length) {
+      clients[0].postMessage({
+        type: 'ERROR',
+        msg: e.message || null,
+        stack: e.error ? e.error.stack : null,
+      });
+    }
+  });
 });
 
 self.addEventListener('unhandledrejection', (e) => {
-  self.clients.matchAll()
-    .then((clients) => {
-      if (clients && clients.length) {
-        clients[0].postMessage({
-          type: 'REJECTION',
-          msg: e.reason ? e.reason.message : null,
-          stack: e.reason ? e.reason.stack : null,
-        });
-      }
-    });
+  self.clients.matchAll().then((clients) => {
+    if (clients && clients.length) {
+      clients[0].postMessage({
+        type: 'REJECTION',
+        msg: e.reason ? e.reason.message : null,
+        stack: e.reason ? e.reason.stack : null,
+      });
+    }
+  });
 });
 
 importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
@@ -48,10 +46,7 @@ const staleWhileRevalidateRegList = [
 ];
 
 workbox.routing.registerRoute(
-  ({ url }) => staleWhileRevalidateRegList.filter(reg => reg.test(url) === true).length > 0,
+  ({ url }) => staleWhileRevalidateRegList.filter((reg) => reg.test(url) === true).length > 0,
   workbox.strategies.staleWhileRevalidate(),
 );
-workbox.routing.registerRoute(
-  '/',
-  workbox.strategies.staleWhileRevalidate(),
-);
+workbox.routing.registerRoute('/', workbox.strategies.staleWhileRevalidate());
