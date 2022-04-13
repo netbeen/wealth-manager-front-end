a5f2b1b298d3d5c5ed00d217457b498ebec9671c
Yang Yang <yangyang.jeff@bytedance.com>
2022-04-13 13:54:44 +0800

    refactor: format

--- .umirc.ts
+++ .umirc.ts
@@ -2,26 +2,25 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   title: 'Wealth Manager',
-    links: [{
-  rel: 'icon',
-  href: '/img/logo.svg',
-}],
+  links: [
+    { rel: 'icon', href: '/img/logo.svg' }
+  ],
   styles: [`https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css`],
-  metas: [{
-  name: 'google-site-verification',
-  content: 'nfOPr4G-D6vPaud2_lZtu7JpzA6FZ71Zy3XV-ekCL8w',
-}, {
-  name: 'keywords',
-  content: 'fund, wealth, manager',
-}, {
-  name: 'description',
-  content: '一站式家庭财务管理平台',
-}],
+  metas: [
+    {
+      name: 'google-site-verification',
+      content: 'nfOPr4G-D6vPaud2_lZtu7JpzA6FZ71Zy3XV-ekCL8w',
+    }, {
+      name: 'keywords',
+      content: 'fund, wealth, manager',
+    }, {
+      name: 'description',
+      content: '一站式家庭财务管理平台',
+    }
+  ],
   webpack5: {},
   chainWebpack: function (config) {
     config.module.rule('mjs-rule').test(/.m?js/).resolve.set('fullySpecified', false);
   },
-  scripts: [
-    { src: '/workboxLoader.js', defer: true },
-  ],
+  scripts: [{ src: '/workboxLoader.js', defer: true }],
 });
