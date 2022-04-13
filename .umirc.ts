940648d0cc5534737df57d4b99287fc306857b50
Yang Yang <yangyang.jeff@bytedance.com>
2022-04-13 13:54:44 +0800

    refactor: refactor config

--- .umirc.ts
+++ .umirc.ts
@@ -1,27 +1,24 @@
-import { IConfig } from 'umi-types';
+import { defineConfig } from 'umi';
 
-// ref: https://umijs.org/config/
-const config: IConfig = {
+export default defineConfig({
   title: 'Wealth Manager',
-  links: [{
-    rel: 'icon',
-    href: '/img/logo.svg',
-  }],
+    links: [{
+  rel: 'icon',
+  href: '/img/logo.svg',
+}],
   styles: [`https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css`],
   metas: [{
-    name: 'google-site-verification',
-    content: 'nfOPr4G-D6vPaud2_lZtu7JpzA6FZ71Zy3XV-ekCL8w',
-  }, {
-    name: 'keywords',
-    content: 'fund, wealth, manager',
-  }, {
-    name: 'description',
-    content: '一站式家庭财务管理平台',
-  }],
+  name: 'google-site-verification',
+  content: 'nfOPr4G-D6vPaud2_lZtu7JpzA6FZ71Zy3XV-ekCL8w',
+}, {
+  name: 'keywords',
+  content: 'fund, wealth, manager',
+}, {
+  name: 'description',
+  content: '一站式家庭财务管理平台',
+}],
   webpack5: {},
   scripts: [
     { src: '/workboxLoader.js', defer: true },
   ],
-};
-
-export default config;
+});
