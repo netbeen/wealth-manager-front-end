796fe3f4be5245f2b701851be80eb6ef25e96503
Yang Yang <yangyang.jeff@bytedance.com>
2022-05-26 23:19:10 +0800

    chore: switch api to aliyun

--- src/globalConst.ts
+++ src/globalConst.ts
@@ -1,7 +1,7 @@
 export const API_PREFIX =
   window.location.hostname === '127.0.0.1'
     ? 'http://127.0.0.1:5003'
-    : 'https://service-0d8643vs-1302187355.hk.apigw.tencentcs.com/test';
+    : 'https://wealth-manager-api.netbeen.top';
 
 export const TOKEN_COOKIE_NAME = 'wealthManagerToken';
 export const ORGANIZATION_COOKIE_NAME = 'wealthManagerOrganization';
